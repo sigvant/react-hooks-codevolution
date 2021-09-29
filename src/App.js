4e6eac4a722ead8ca84e10e9bfc172badda9714b
sigvant <bnsviv@gmail.com>
2021-09-29 16:40:07 -0300

    end of usestate with object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import HookCounterThree from './components/HookCounterThree';
 // import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
+// import HookCounter from './components/HookCounter';
+// import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter/> */}
-      <HookCounter/>
+      <HookCounterThree/>
     </div>
   );
 }
