5a226daab526d6a526bf9df74e6795caa32ec9e4
sigvant <bnsviv@gmail.com>
2021-09-29 19:34:25 -0300

    run useeffect with conditions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterThree from './components/HookCounterThree';
-// import ClassCounter from './components/ClassCounter';
-// import HookCounter from './components/HookCounter';
-// import HookCounterTwo from './components/HookCounterTwo';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounter/> */}
-      <HookCounterFour/>
+          {/* <ClassCounterOne/> */}
+          <HookCounterOne/>
     </div>
+
   );
 }
 
