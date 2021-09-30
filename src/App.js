ffc3559dfe4301be43509fc30674f53b4380495a
sigvant <bnsviv@gmail.com>
2021-09-30 10:22:47 -0300

    final lesson on use context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import IntervalClassCounter from './components/IntervalClassCounter';
-import IntervalHookCounter from './components/IntervalHookCounter';
-
+import DataFetching from './components/DataFetching';
 
 function App() {
   return (
     <div className="App">
-      <IntervalClassCounter/>
-      <IntervalHookCounter/>
+      <DataFetching/>
     </div>
   );
 }
