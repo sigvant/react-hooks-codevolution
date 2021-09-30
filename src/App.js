e6b826247783c3a6d5cb9095f767b2d630ebd346
sigvant <bnsviv@gmail.com>
2021-09-30 09:00:38 -0300

    suseeffect hook final lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import ClassMouse from './components/ClassMouse';
-import HookMouse from './components/HookMouse';
-import MouseContainer from './components/MouseContainer';
+import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassMouse/> */}
-      {/* <HookMouse/> */}
-      <MouseContainer/>
+      <IntervalClassCounter/>
+      <IntervalHookCounter/>
     </div>
   );
 }
