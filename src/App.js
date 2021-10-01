6a4d4734df336ecf9af8d61acb45090c232740e7
sigvant <bnsviv@gmail.com>
2021-10-01 08:59:18 -0300

    final commit on reduce with context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
+import React from 'react';
 import CompA from './components/CompA';
 import CompB from './components/CompB';
 import CompC from './components/CompC';
+import {useReducer} from 'react'
+
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+  switch(action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <CompA/>
-      <CompB/>
-      <CompC/>
-    </div>
+    <CountContext.Provider value={{ countState: count, countDispatch: dispatch}}>
+      <div className="App">
+        Count - {count}
+        <CompA/>
+        <CompB/>
+        <CompC/>
+      </div>
+    </CountContext.Provider>
   );
 }
 
--- src/components/CompA.js
+++ src/components/CompA.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CountContext } from '../App'
 
 function CompA() {
+    const countContext = useContext(CountContext)
     return (
         <div>
-            <h2>Component A</h2>
+            <h2>Component A - {countContext.countState}</h2>
+            {/* same thing here for countContext.countState. That's how we called it
+            when we gave it a name in the value object at the context provider */}
+            <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
+            {/* this name 'countCountext.countDispatch' comes because in APP.js we provided the name
+            countDispatch in the object of the value attribute of the context provider. */}
+            <button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
+            <button onClick={() => countContext.countDispatch('reset')}>Reset</button>
         </div>
     )
 }
