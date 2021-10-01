73eb076f33b45ea0f41f6e032d3e7ee58d765e13
sigvant <bnsviv@gmail.com>
2021-10-01 09:35:50 -0300

    final lesson on fetching with useReducer and useEffect

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import './App.css';
 import React from 'react';
-import CompA from './components/CompA';
-import CompB from './components/CompB';
-import CompC from './components/CompC';
 import {useReducer} from 'react'
-
-export const CountContext = React.createContext()
-
-const initialState = 0
-const reducer = (state, action) => {
-  switch(action) {
-    case 'increment':
-      return state + 1
-    case 'decrement':
-      return state - 1
-    case 'reset':
-      return initialState
-    default:
-      return state
-  }
-}
+import DataFetchingOne from './components/DataFetchingOne';
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <CountContext.Provider value={{ countState: count, countDispatch: dispatch}}>
       <div className="App">
-        Count - {count}
-        <CompA/>
-        <CompB/>
-        <CompC/>
+        <DataFetchingOne/>
       </div>
-    </CountContext.Provider>
   );
 }
 
---
+++ src/components/DataFetchingOne.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+
+function DataFetchingOne() {
+
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+            .then(res => {
+                setLoading(false)
+                setPost(res.data)
+                setError('')
+            })
+            .catch(err => {
+                setLoading(false)
+                setPost({})
+                setError('Something went wrong')
+            })
+    }, [])
+
+    // only runs once
+
+    return (
+        <div>
+            {loading ? 'Loading' : post.title}
+            {error ? error : null}
+        </div>
+    )
+}
+
+export default DataFetchingOne
+
