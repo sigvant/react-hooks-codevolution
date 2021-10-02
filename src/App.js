9c7ea3875216b381441f80e2b7c12f10112dc6a0
sigvant <bnsviv@gmail.com>
2021-10-02 08:09:08 -0300

    use ref hook part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Counter from './components/Counter';
+import FocusInput from './components/FocusInput';
 
 function App() {
   return (
       <div className="App">
-        <Counter/>
+        <FocusInput/>
       </div>
   );
 }
